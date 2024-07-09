2d19d7da1803b8002e9f614df7061e537876576b
HamzaXgrid <hamza.arshad@xgrid.co>
2024-07-09 17:02:03 +0500

    DS status 3

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -14,33 +14,32 @@ def daemonset_status_enricher(event: DaemonSetEvent):
     """
     ds: DaemonSet = event.get_daemonset()
     print("Daemonsets",ds)
-    # Define the tolerations to be added
-    # new_tolerations = [
-    #     {
-    #         "key": "key1",
-    #         "operator": "Equal",
-    #         "value": "value1",
-    #         "effect": "NoSchedule"
-    #     },
-    #     {
-    #         "key": "key2",
-    #         "operator": "Exists",
-    #         "effect": "NoExecute",
-    #         "tolerationSeconds": 3600
-    #     }
-    # ]
+    new_tolerations = [
+        {
+            "key": "key1",
+            "operator": "Equal",
+            "value": "value1",
+            "effect": "NoSchedule"
+        },
+        {
+            "key": "key2",
+            "operator": "Exists",
+            "effect": "NoExecute",
+            "tolerationSeconds": 3600
+        }
+    ]
     
-    # # Add tolerations if they do not already exist
-    # if not does_daemonset_have_toleration(ds, new_tolerations):
-    #     if not ds.spec.template.spec.tolerations:
-    #         ds.spec.template.spec.tolerations = new_tolerations
-    #     else:
-    #         for toleration in new_tolerations:
-    #             if toleration not in ds.spec.template.spec.tolerations:
-    #                 ds.spec.template.spec.tolerations.append(toleration)
+    # Add tolerations if they do not already exist
+    if not does_daemonset_have_toleration(ds, new_tolerations):
+        if not ds.spec.template.spec.tolerations:
+            ds.spec.template.spec.tolerations = new_tolerations
+        else:
+            for toleration in new_tolerations:
+                if toleration not in ds.spec.template.spec.tolerations:
+                    ds.spec.template.spec.tolerations.append(toleration)
     
-    # # Update the DaemonSet with the new tolerations
-    # ds.update()
+    # Update the DaemonSet with the new tolerations
+    ds.update()
 
 @action
 def delete_pod(event: PodEvent):
