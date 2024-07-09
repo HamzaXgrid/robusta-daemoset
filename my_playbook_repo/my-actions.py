00cda4e1a4350f55a48eb0fe1ae8c3c6cbd6e403
HamzaXgrid <hamza.arshad@xgrid.co>
2024-07-09 16:30:23 +0500

    DS status

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -1,12 +1,44 @@
-from robusta.api import ActionException, ErrorCodes, PodEvent, action
-
+from hikaru.model.rel_1_26 import DaemonSet
+from robusta.api import (
+    DaemonSetEvent,
+    action,
+    does_daemonset_have_toleration,
+)
 
 @action
-def delete_pod(event: PodEvent):
+def daemonset_status_enricher(event: DaemonSetEvent):
     """
-    Deletes a pod
-    """
-    if not event.get_pod():
-        raise ActionException(ErrorCodes.RESOURCE_NOT_FOUND, "Failed to get the pod for deletion")
+    Enrich the finding with daemon set stats.
 
-    event.get_pod().delete()
+    Includes recommendations for the identified cause.
+    """
+    ds: DaemonSet = event.get_daemonset()
+    print("Daemonsets",ds)
+    # Define the tolerations to be added
+    # new_tolerations = [
+    #     {
+    #         "key": "key1",
+    #         "operator": "Equal",
+    #         "value": "value1",
+    #         "effect": "NoSchedule"
+    #     },
+    #     {
+    #         "key": "key2",
+    #         "operator": "Exists",
+    #         "effect": "NoExecute",
+    #         "tolerationSeconds": 3600
+    #     }
+    # ]
+    
+    # # Add tolerations if they do not already exist
+    # if not does_daemonset_have_toleration(ds, new_tolerations):
+    #     if not ds.spec.template.spec.tolerations:
+    #         ds.spec.template.spec.tolerations = new_tolerations
+    #     else:
+    #         for toleration in new_tolerations:
+    #             if toleration not in ds.spec.template.spec.tolerations:
+    #                 ds.spec.template.spec.tolerations.append(toleration)
+    
+    # # Update the DaemonSet with the new tolerations
+    # ds.update()
+
