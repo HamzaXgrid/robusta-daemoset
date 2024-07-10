d3e2f1d8ba00d3ff590a7df65d9279b9f687ef37
HamzaXgrid <hamza.arshad@xgrid.co>
2024-07-10 13:56:24 +0500

    status 04

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -1,9 +1,9 @@
-from hikaru.model.rel_1_26 import DaemonSet
+from hikaru.model.rel_1_26 import DaemonSet, NodeList, Toleration
 from robusta.api import (
     DaemonSetEvent,
-    action, PodEvent
-
+    action
 )
+from kubernetes import client, config
 
 @action
 def daemonset_status_enricher(event: DaemonSetEvent):
@@ -14,32 +14,40 @@ def daemonset_status_enricher(event: DaemonSetEvent):
     """
     print("---------------------------- Daemon Set -------------------------------------")
     DaemonSet = event.get_daemonset()
-    #print("daemonset is ",DaemonSet)
-    # Define the tolerations to be added
-    new_tolerations = [
-        {
-            "key": "key1",
-            "operator": "Equal",
-            "value": "value1",
-            "effect": "NoSchedule"
-        },
-        {
-            "key": "key2",
-            "operator": "Exists",
-            "effect": "NoExecute",
-            "tolerationSeconds": 3600
-        }
-    ]
+
+    # Load Kubernetes config
+    # config.load_incluster_config()
+    # v1 = client.CoreV1Api()
     
+    # Get all nodes
+    nodes = NodeList.readNamespacedNodeList()
+    print(nodes)
+    # Extract taints from nodes
+    new_tolerations = []
+
+    for node in nodes.items:
+        if node.spec.taints:
+            for taint in node.spec.taints:
+                new_tolerations.append(
+                    Toleration(
+                        key=taint.key,
+                        operator="Equal" if taint.value else "Exists",
+                        value=taint.value,
+                        effect=taint.effect,
+                        tolerationSeconds=taint.tolerationSeconds
+                    )
+                )
+    print("-----------------------------------")
+    print(new_tolerations)
     # # Check if tolerations already exist and add if they do not
-    existing_tolerations = DaemonSet.spec.template.spec.tolerations or []
-    for new_toleration in new_tolerations:
-        if new_toleration not in existing_tolerations:
-            existing_tolerations.append(new_toleration)
-    print(existing_tolerations)
-    print("Tolerations Added")
-    DaemonSet.spec.template.spec.tolerations = existing_tolerations
+    # existing_tolerations = DaemonSet.spec.template.spec.tolerations or []
+    # for new_toleration in new_tolerations:
+    #     if new_toleration not in existing_tolerations:
+    #         existing_tolerations.append(new_toleration)
+    # print(existing_tolerations)
+    # print("Tolerations Added")
+    # DaemonSet.spec.template.spec.tolerations = existing_tolerations
     
-    # Update the DaemonSet with the new tolerations
-    DaemonSet.update()
+    # # Update the DaemonSet with the new tolerations
+    # DaemonSet.update()
 
