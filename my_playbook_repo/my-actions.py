676014876d1724f82f7567029205851b7402b9aa
HamzaXgrid <hamza.arshad@xgrid.co>
2024-07-15 17:16:37 +0500

    PV task added 1

--- my_playbook_repo/my-actions.py
+++ my_playbook_repo/my-actions.py
@@ -1,54 +1,96 @@
-from hikaru.model.rel_1_26 import Toleration
-from robusta.api import (
-    DaemonSetEvent,
-    action
-)
+from robusta.api import ActionParams, PersistentVolumeEvent, action
 from kubernetes import client, config
 
+class ResizePvParams(ActionParams):
+    new_size: str
+
 @action
-def daemonset_status_enricher(event: DaemonSetEvent):
-    """
-    Enrich the finding with daemon set stats.
-
-    Includes recommendations for the identified cause.
-    """
-    daemonSet = event.get_daemonset()
-
-    # Load Kubernetes config
+def resize_pv(event: PersistentVolumeEvent, params: ResizePvParams):
     config.load_incluster_config()
     v1 = client.CoreV1Api()
-    
-    # Get all nodes
-    nodes = v1.list_node()
+    persistentVolume = event.get_persistentvolume()
+    #pvc_name = event.get_pvc_name()
+    #namespace = event.get_pvc_namespace()
+    print("PV is: ", persistentVolume)
+    # if not pvc_name or not namespace:
+    #     event.add_enrichment([{
+    #         "type": "markdown",
+    #         "content": f"PVC name or namespace not found in the event."
+    #     }])
+    #     return
 
-    # Extract taints from nodes
-    newTolerations = []
+    # # Get the PVC
+    # pvc = v1.read_namespaced_persistent_volume_claim(name=pvc_name, namespace=namespace)
 
-    for node in nodes.items:
-        labels = node.metadata.labels
-        if "node-role.kubernetes.io/master" not in labels:
-            if node.spec.taints:
-                for taint in node.spec.taints:
-                    newTolerations.append(
-                        Toleration(
-                            key=taint.key,
-                            operator="Equal" if taint.value else "Exists",
-                            value=taint.value,
-                            effect=taint.effect,
-                        )
-                    )
-    print("-----------------------------------")
-    print(newTolerations)
-    # # Check if tolerations already exist and add if they do not
-    existingTolerations = daemonSet.spec.template.spec.tolerations or []
-    for new_toleration in newTolerations:
-        if new_toleration not in existingTolerations:
-            print("Existing tolerations are : ",existingTolerations)
-            print("New tolerations are : ",new_toleration)
-            existingTolerations.append(new_toleration)
-    print("Tolerations Added")
-    daemonSet.spec.template.spec.tolerations = existingTolerations
+    # # Update the PVC size
+    # pvc.spec.resources.requests['storage'] = params.new_size
+
+    # # Patch the PVC
+    # v1.patch_namespaced_persistent_volume_claim(name=pvc_name, namespace=namespace, body=pvc)
+
+    # # Get the corresponding PV
+    # pv_name = pvc.spec.volume_name
+    # pv = v1.read_persistent_volume(name=pv_name)
+
+    # # Update the PV size
+    # pv.spec.capacity['storage'] = params.new_size
+
+    # # Patch the PV
+    # v1.patch_persistent_volume(name=pv_name, body=pv)
+
+    # event.add_enrichment([{
+    #     "type": "markdown",
+    #     "content": f"Resized PVC `{pvc_name}` in namespace `{namespace}` to `{params.new_size}`"
+    # }])
+
+# from hikaru.model.rel_1_26 import Toleration
+# from robusta.api import (
+#     DaemonSetEvent,
+#     action
+# )
+# from kubernetes import client, config
+
+# @action
+# def daemonset_status_enricher(event: DaemonSetEvent):
+#     """
+#     Enrich the finding with daemon set stats.
+
+#     Includes recommendations for the identified cause.
+#     """
+#     daemonSet = event.get_daemonset()
+
+#     # Load Kubernetes config
+#     config.load_incluster_config()
+#     v1 = client.CoreV1Api()
     
-    # Update the DaemonSet with the new tolerations
-    daemonSet.update()
+#     # Get all nodes
+#     nodes = v1.list_node()
+
+#     # Extract taints from nodes
+#     newTolerations = []
+
+#     for node in nodes.items:
+#         labels = node.metadata.labels
+#         if "node-role.kubernetes.io/master" not in labels:
+#             if node.spec.taints:
+#                 for taint in node.spec.taints:
+#                     newTolerations.append(
+#                         Toleration(
+#                             key=taint.key,
+#                             operator="Equal" if taint.value else "Exists",
+#                             value=taint.value,
+#                             effect=taint.effect,
+#                         )
+#                     )
+
+#     # # Check if tolerations already exist and add if they do not
+#     existingTolerations = daemonSet.spec.template.spec.tolerations or []
+#     for newToleration in newTolerations:
+#         if newToleration not in existingTolerations:
+#             existingTolerations.append(newToleration)
+#     daemonSet.spec.template.spec.tolerations = existingTolerations
+    
+#     # Update the DaemonSet with the new tolerations
+#     daemonSet.update()
+
 
